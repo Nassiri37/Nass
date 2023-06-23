dd0a50fc1b3e8dc3b744a6db49a51ec4f32ff95c
Nass <105737892+Nassiri37@users.noreply.github.com>
2023-06-23 01:54:23 -0400

    Update cl_rentals.lua

--- nass-rentals/client/cl_rentals.lua
+++ nass-rentals/client/cl_rentals.lua
@@ -3,8 +3,12 @@ local QBCore = exports['qb-core']:GetCoreObject()
 local rentalPlate = Config.PlateName
 local pedModel = Config.BikePed
 local pedModel2 = Config.BoatPed
+local pedModel3 = Config.dBikePed
+local pedModel4 = Config.CarPed
 local bikecoords = Config.BikePedCoords
 local boatcoords = Config.BoatPedCoords
+local dBikecoords = Config.dBikePedCoords
+local carcoords = Config.CarPedCoords
 
 
 --------------------BIKE RENTAL--------------------
@@ -61,6 +65,7 @@ AddEventHandler("nass-rental:returnbike", function()
         if string.find(tostring(plate), rentalPlate) ~= nil then
             TriggerServerEvent('nass-rental:server:bikepayreturn', vehName)
             QBCore.Functions.DeleteVehicle(vehicle)
+            QBCore.Functions.Notify("You returned the vehicle!.", "success")
         else
             QBCore.Functions.Notify("This is not a rented vehicle.", "error")
         end
@@ -135,12 +140,14 @@ CreateThread(function()
                 options = {
                     {
                         icon = 'fas fa-circle',
+                        num = 1,
                         label = 'Rent Bike',
                         type = 'client',
                         event = 'nass-rental:bikelist',
                     },
                     {
                         icon = 'fas fa-circle',
+                        num = 2,
                         label = 'Return Bike',
                         type = 'client',
                         event = 'nass-rental:returnbike',
@@ -229,6 +236,7 @@ RegisterNetEvent("nass-rental:boatlist", function()
           if string.find(tostring(plate), rentalPlate) ~= nil then
               TriggerServerEvent('nass-rental:server:boatpayreturn', vehName)
               QBCore.Functions.DeleteVehicle(vehicle)
+              QBCore.Functions.Notify("You returned the boat!.", "success")
           else
               QBCore.Functions.Notify("This is not a rented Boat.", "error")
           end
@@ -304,6 +312,7 @@ RegisterNetEvent("nass-rental:boatlist", function()
                 options = {
                     {
                         icon = 'fas fa-circle',
+                        num = 1,
                         label = 'Rent Boat',
                         type = 'client',
                         event = 'nass-rental:boatlist',
@@ -311,6 +320,7 @@ RegisterNetEvent("nass-rental:boatlist", function()
                     {
                         icon = 'fas fa-circle',
                         label = 'Return Boat',
+                        num = 2,
                         type = 'client',
                         event = 'nass-rental:returnboat',
                     },
@@ -337,3 +347,341 @@ end)
   
 
 --------------------END OF BOATS--------------------
+
+
+--------------------START OF DIRT BIKES--------------------
+--qb-menu
+RegisterNetEvent("nass-rental:dbikeslist", function()
+    for i = 1, #Config.dBikeList do
+    
+              local MenuOptions = {
+                  {
+                      header = "Dirt Bike Rental",
+                      isMenuHeader = true
+                  },
+              }
+              for k, v in pairs(Config.dBikeList) do
+                    MenuOptions[#MenuOptions+1] = {
+                      header = "<h8>"..v.name.."</h>",
+                txt = "$"..v.price..".00",
+                      params = {
+                  event = "nass-rental:attemptdbikespawn",
+                  args = {
+                    id = v.model,
+                    price = v.price
+                  }
+                      }
+                  }
+              end
+              exports['qb-menu']:openMenu(MenuOptions)
+        end
+  end)
+  
+  
+  
+  
+  RegisterNetEvent("nass-rental:attemptdbikespawn", function(vehicle)
+      TriggerServerEvent("nass-rental:attemptdBikePurchase",vehicle.id, vehicle.price)
+  end)
+  
+  RegisterNetEvent("nass-rental:attemptdbikespawnfail", function()
+    QBCore.Functions.Notify("Not enough money.", "error")
+  end)
+  
+  
+  
+  
+  --Delete The Vehicle
+  RegisterNetEvent("nass-rental:returndbike")
+  AddEventHandler("nass-rental:returndbike", function()
+      local playerPed = PlayerPedId()
+      local vehicle = GetVehiclePedIsIn(playerPed, false)
+      if vehicle ~= 0 then
+          local plate = GetVehicleNumberPlateText(vehicle)
+          if string.find(tostring(plate), rentalPlate) ~= nil then
+              TriggerServerEvent('nass-rental:server:dbikepayreturn', vehName)
+              QBCore.Functions.DeleteVehicle(vehicle)
+              QBCore.Functions.Notify("You returned the vehicle!.", "success")
+          else
+              QBCore.Functions.Notify("This is not a rented Dirt Bike.", "error")
+          end
+      else
+          QBCore.Functions.Notify("You are not on a Dirt Bike.", "error")
+      end
+  end)
+  
+  
+  
+  --Spawn the vehicle
+  RegisterNetEvent("nass-rental:dbikespawn", function(data, cb)
+    local model = data
+  
+    local closestDist = 10000
+    local closestSpawn = nil
+    local pcoords = GetEntityCoords(PlayerPedId())
+    
+    for i, v in ipairs(Config.dBikeSpawn) do
+        local dist = #(v.dBikeSpawn.coords - pcoords)
+    
+        if dist < closestDist then
+            closestDist = dist
+            closestSpawn = v.dBikeSpawn
+        end
+    end
+  
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Citizen.Wait(0)
+    end
+    SetModelAsNoLongerNeeded(model)
+  
+    QBCore.Functions.SpawnVehicle(model, function(veh)
+      SetVehicleNumberPlateText(veh, rentalPlate)
+      SetEntityHeading(veh, closestSpawn.heading)
+      exports[Config.FuelScript]:SetFuel(veh, 100.0)
+      TaskWarpPedIntoVehicle(PlayerPedId(), veh, -1)
+      SetEntityAsMissionEntity(veh, true, true)
+      TriggerEvent("vehiclekeys:client:SetOwner", QBCore.Functions.GetPlate(veh))
+      SetVehicleEngineOn(veh, true, true)
+      CurrentPlate = QBCore.Functions.GetPlate(veh)
+    end, closestSpawn.coords, true)
+  
+    local timeout = 10
+    while not NetworkDoesEntityExistWithNetworkId(veh) and timeout > 0 do
+        timeout = timeout - 1
+        Wait(1000)
+    end
+  end)
+  
+  
+  
+  CreateThread(function()
+    if Config.EnabledBikeRental then
+        QBCore.Functions.LoadModel(pedModel3)
+
+        for _, coords in ipairs(dBikecoords) do
+            while not HasModelLoaded(pedModel3) do
+                Wait(100)
+            end
+
+            local ped3 = CreatePed(0, pedModel3, coords.x, coords.y, coords.z - 1.0, coords.w, true, true)
+            FreezeEntityPosition(ped3, true)
+            SetEntityInvincible(ped3, true)
+            SetBlockingOfNonTemporaryEvents(ped3, true)
+
+            exports['qb-target']:AddTargetEntity(ped3, {
+                options = {
+                    {
+                        icon = 'fas fa-circle',
+                        num = 1,
+                        label = 'Rent Dirt Bike',
+                        type = 'client',
+                        event = 'nass-rental:dbikeslist',
+                    },
+                    {
+                        icon = 'fas fa-circle',
+                        label = 'Return Dirt Bike',
+                        num = 2,
+                        type = 'client',
+                        event = 'nass-rental:returndbike',
+                    },
+                },
+                distance = 2.0
+            })
+        end
+
+        if Config.dBikeBlips then
+            for _, coords in ipairs(Config.dBikePedCoords) do
+                local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+                SetBlipSprite(blip, 815)
+                SetBlipAsShortRange(blip, true)
+                SetBlipScale(blip, 0.5)
+                SetBlipColour(blip, 5)
+                BeginTextCommandSetBlipName("STRING")
+                AddTextComponentString(Config.dBikeBlipName)
+                EndTextCommandSetBlipName(blip)
+            end
+        end
+    end
+end)
+
+
+--------------------END OF DIRT BIKES--------------------
+
+
+--------------------START OF CAR--------------------
+--qb-menu
+RegisterNetEvent("nass-rental:carlist", function()
+    for i = 1, #Config.CarList do
+    
+              local MenuOptions = {
+                  {
+                      header = "Vehicle Rental",
+                      isMenuHeader = true
+                  },
+              }
+              for k, v in pairs(Config.CarList) do
+                    MenuOptions[#MenuOptions+1] = {
+                      header = "<h8>"..v.name.."</h>",
+                txt = "$"..v.price..".00",
+                      params = {
+                  event = "nass-rental:attemptcarspawn",
+                  args = {
+                    id = v.model,
+                    price = v.price
+                  }
+                      }
+                  }
+              end
+              exports['qb-menu']:openMenu(MenuOptions)
+        end
+  end)
+  
+  
+  
+  
+  RegisterNetEvent("nass-rental:attemptcarspawn", function(vehicle)
+      TriggerServerEvent("nass-rental:attemptCarPurchase",vehicle.id, vehicle.price)
+  end)
+  
+  RegisterNetEvent("nass-rental:attemptcarspawnfail", function()
+    QBCore.Functions.Notify("Not enough money.", "error")
+  end)
+  
+  
+  
+  
+ 
+
+    RegisterNetEvent("nass-rental:carpayreturn", function()
+        local car = GetVehiclePedIsIn(PlayerPedId(),true)
+      
+        if car ~= 0 then
+          local plate = GetVehicleNumberPlateText(car)
+          local vehname = string.lower(GetDisplayNameFromVehicleModel(GetEntityModel(car)))
+          if string.find(tostring(plate), Config.PlateName) then
+            QBCore.Functions.TriggerCallback('nass-rentals:server:hasrentalpapers', function(HasItem)
+              if HasItem then
+                TriggerServerEvent('nass-rentals:server:removerentalpapers', 'rentalpapers', 1)
+                TriggerServerEvent('nass-rental:server:carpayreturn',vehname)
+                QBCore.Functions.DeleteVehicle(car)
+                QBCore.Functions.Notify("You returned the vehicle!.", "success")
+              else
+                QBCore.Functions.Notify("I cannot take a vehicle without its papers.", "error")
+              end
+            end)
+          else
+            QBCore.Functions.Notify("This is not a rented vehicle.", "error")
+          end
+      
+        else
+          QBCore.Functions.Notify("I don't see any rented vehicle, make sure its nearby.", "error")
+        end
+      end)
+  
+  
+  --Spawn the vehicle
+  RegisterNetEvent("nass-rental:carspawn", function(data, cb)
+    local model = data
+  
+    local closestDist = 10000
+    local closestSpawn = nil
+    local pcoords = GetEntityCoords(PlayerPedId())
+    
+    for i, v in ipairs(Config.CarSpawn) do
+        local dist = #(v.CarSpawn.coords - pcoords)
+    
+        if dist < closestDist then
+            closestDist = dist
+            closestSpawn = v.CarSpawn
+        end
+    end
+  
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Citizen.Wait(0)
+    end
+    SetModelAsNoLongerNeeded(model)
+  
+    QBCore.Functions.SpawnVehicle(model, function(veh)
+      SetVehicleNumberPlateText(veh, rentalPlate ..tostring(math.random(1000, 9999)))
+      SetEntityHeading(veh, closestSpawn.heading)
+      exports[Config.FuelScript]:SetFuel(veh, 100.0)
+      TaskWarpPedIntoVehicle(PlayerPedId(), veh, -1)
+      SetEntityAsMissionEntity(veh, true, true)
+      TriggerEvent("vehiclekeys:client:SetOwner", QBCore.Functions.GetPlate(veh))
+      SetVehicleEngineOn(veh, true, true)
+      CurrentPlate = QBCore.Functions.GetPlate(veh)
+    end, closestSpawn.coords, true)
+  
+    local plateText = GetVehicleNumberPlateText(veh)
+    TriggerServerEvent("nass-rental:giverentalpaperServer",model ,plateText)
+
+    local timeout = 10
+    while not NetworkDoesEntityExistWithNetworkId(veh) and timeout > 0 do
+        timeout = timeout - 1
+        Wait(1000)
+    end
+  end)
+  
+  
+  
+  CreateThread(function()
+    if Config.EnableCarRental then
+        QBCore.Functions.LoadModel(pedModel4)
+
+        for _, coords in ipairs(Config.CarPedCoords) do
+            while not HasModelLoaded(pedModel4) do
+                Wait(100)
+            end
+
+            local ped4 = CreatePed(0, pedModel4, coords.x, coords.y, coords.z - 1.0, coords.w, true, true)
+            FreezeEntityPosition(ped4, true)
+            SetEntityInvincible(ped4, true)
+            SetBlockingOfNonTemporaryEvents(ped4, true)
+
+            exports['qb-target']:AddTargetEntity(ped4, {
+                options = {
+                    {
+                        icon = 'fas fa-circle',
+                        num = 1,
+                        label = 'Rent a vehicle',
+                        type = 'client',
+                        event = 'nass-rental:carlist',
+                    },
+                    {
+                        icon = 'fas fa-circle',
+                        label = 'Return a vehicle',
+                        num = 2,
+                        type = 'client',
+                        event = 'nass-rental:carpayreturn',
+                    },
+                },
+                distance = 2.0
+            })
+        end
+
+        if Config.CarBlips then
+            for _, coords in ipairs(Config.CarPedCoords) do
+                local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+                SetBlipSprite(blip, 815)
+                SetBlipAsShortRange(blip, true)
+                SetBlipScale(blip, 0.5)
+                SetBlipColour(blip, 5)
+                BeginTextCommandSetBlipName("STRING")
+                AddTextComponentString(Config.CarBlipName)
+                EndTextCommandSetBlipName(blip)
+            end
+        end
+    end
+end)
+
+
+
+local PlayerName = nil 
+RegisterNetEvent("nass-rental:giverentalpaper", function(model, plate, name) 
+    local info = {
+        date = "Model : "..tostring(model).." | Plate : "..tostring(plate)..""
+    }
+    TriggerServerEvent('QBCore:Server:AddItem', "rentalpapers", 1, info)
+end)
